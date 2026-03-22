9e3d38aade69148c870735e31cc1cf55d54fab58
mizoqassem64-crypto <mizoqassem64@gmail.com>
2026-03-22 22:23:55 +0200

    Add AegisDeviceAdminReceiver for device admin features

--- AegisDeviceAdminReceiver.java
+++ AegisDeviceAdminReceiver.java
@@ -6,13 +6,11 @@ import android.content.Intent;
 import android.widget.Toast;
 
 public class AegisDeviceAdminReceiver extends DeviceAdminReceiver {
-    
     @Override
     public void onEnabled(Context context, Intent intent) {
         super.onEnabled(context, intent);
         Toast.makeText(context, "💀 Aegis Shield: Admin Access Granted!", Toast.LENGTH_LONG).show();
     }
-
     @Override
     public void onDisabled(Context context, Intent intent) {
         super.onDisabled(context, intent);
